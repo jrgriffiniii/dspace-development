d0210f5ee2029e6a42bf5d1c83b797afb766b3b9
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-11-06 09:07:36 -0500

    wip

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2025 The Trustees of Princeton University"
 author = "Princeton University Library"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
