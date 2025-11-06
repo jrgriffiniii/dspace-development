f86f3011a6d793814b658c72998b69233a2f842a
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-11-06 08:12:27 -0500

    Upgrading the codebase for readthedocs v2 configuration settings

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'pulibrary/dspace'
-copyright = '2021 The Trustees of Princeton University'
-author = 'Princeton University Library'
+project = "pulibrary/dspace"
+copyright = "2025 The Trustees of Princeton University"
+author = "Princeton University Library"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,22 +30,17 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'recommonmark'
-]
+extensions = ["recommonmark"]
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown'
-}
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,16 +48,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_logo = 'images/pulibrary_dspace_logo.svg'
-html_style = 'default.css'
+html_logo = "images/pulibrary_dspace_logo.svg"
+html_style = "default.css"
 
 # This is needed for publishing to Read the Docs
 # Please see https://github.com/readthedocs/readthedocs.org/issues/2569
-master_doc = 'index'
+master_doc = "index"
